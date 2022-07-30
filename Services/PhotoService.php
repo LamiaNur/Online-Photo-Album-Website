a982b7be13040dabd8ecb1ddba9e1abeadde5cd8
LamiaNur <109430976+LamiaNur@users.noreply.github.com>
2022-07-30 23:38:31 +0600

    like,status,comment added

--- Services/PhotoService.php
+++ Services/PhotoService.php
@@ -127,4 +127,124 @@
     //$ans = DeletePhotoById(2);
     //$ans = DeletePhotosByAlbumId(4);
     // print_r($ans != null? $ans : "null");
+    /********************Statuts Functionalities******************/
+    function CreateStatus($status, $numOfLikes, $photoId, $createdBy) {
+        $createdAt = date('Y-m-d H:i:s', time());
+        //$query = "insert into statusmodel(Status,NumberOfLikes,PhotoId,CreatedAt,UserId) values ('$status', '$numOfLikes','$photoId',$createdAt','$createdBy')";
+        $query = "insert into statusmodel(Status, NumberOfLikes, PhotoId, CreatedAt,UserId) VALUES ('$status', '$numOfLikes', '$photoId', '$createdAt', '$createdBy')";
+        $queryResult = ExecuteQuery($query);
+        if ($queryResult) return 1;
+        return null;
+    }
+    function GetStatusById($id) {
+        $query = "select * from statusmodel where Id='$id'";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $status = $queryResult->fetch_assoc();
+            return $status;
+        }
+        return null;
+    }
+    function GetAllStatusByUserId($userId) {
+        $query = "select * from statusmodel where UserId='$userId'";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $arr = array();
+            while ($row = $queryResult->fetch_assoc()) {
+                $arr[] = $row;
+            }
+            return $arr;
+        }
+        return null;
+    }
+    function GetAllStatus() {
+        $query = "select * from statusmodel order by Id desc";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $arr = array();
+            while ($row = $queryResult->fetch_assoc()) {
+                $arr[] = $row;
+            }
+            return $arr;
+        }
+        return null;
+    }
+    function UpdateStatus($id, $status, $numOfLikes) {
+        $query = "update statusmodel set Status='$status',NumberOfLikes='$numOfLikes' where Id='$id'";
+        $queryResult = ExecuteQuery($query);
+        if ($queryResult) return GetStatusById($id);
+        return null; 
+    }
+    // $ans = CreateStatus("hello", "0", "1", "1");
+    //$ans = GetAllStatusByUserId(1);
+    //print_r($ans != null? $ans : "null");
+    /********************Comments Functionalities******************/
+    function CreateComment($comment, $statusId, $whoCommented) {
+        $createdAt = date('Y-m-d H:i:s', time());
+        $query = "insert into commentmodel(Comment, StatusId, CreatedAt, WhoCommented) VALUES ('$comment', '$statusId', '$createdAt', '$whoCommented')";
+        $queryResult = ExecuteQuery($query);
+        if ($queryResult) return 1;
+        return null;
+    }
+    function GetCommentById($id) {
+        $query = "select * from commentmodel where Id='$id'";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $comment = $queryResult->fetch_assoc();
+            return $comment;
+        }
+        return null;
+    }
+    function GetCommentsByStatusId($statusId) {
+        $query = "select * from commentmodel where StatusId='$statusId' order by Id desc";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $arr = array();
+            while ($row = $queryResult->fetch_assoc()) {
+                $arr[] = $row;
+            }
+            return $arr;
+        }
+        return null;
+    }
+
+    /********************Like Functionalities******************/
+    function CreateLike($statusId, $whoLiked) {
+        $createdAt = date('Y-m-d H:i:s', time());
+        $query = "insert into likemodel(StatusId, WhoLiked, CreatedAt) VALUES ( '$statusId' , '$whoLiked', '$createdAt')";
+        $queryResult = ExecuteQuery($query);
+        if ($queryResult) return 1;
+        return null;
+    }
+    function GetLikeById($id) {
+        $query = "select * from likemodel where Id='$id'";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $like = $queryResult->fetch_assoc();
+            return $like;
+        }
+        return null;
+    }
+    function GetLikesByStatusId($statusId) {
+        $query = "select * from likemodel where StatusId='$statusId' order by Id desc";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $arr = array();
+            while ($row = $queryResult->fetch_assoc()) {
+                $arr[] = $row;
+            }
+            return $arr;
+        }
+        return null;
+    }
+    function GetLikeByStatusIdAndUserId($statusId, $userId) {
+        $query = "select * from likemodel where StatusId='$statusId' and WhoLiked ='$userId' order by Id desc";
+        $queryResult = ExecuteQuery($query);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $like = $queryResult->fetch_assoc();
+            return $like;
+        }
+        return null;
+    }
+
 ?>
